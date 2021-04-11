edc2fc3f6c7da4caefba68d0ef9d5553787caec0
KentRegs <mkentregalado@gmail.com>
2021-04-11 09:45:42 +0800

    tweaks

--- gcash-mini-hw-template-master/gcash-mini-hw-template-master/user-management/src/main/java/ph/apper/App.java
+++ gcash-mini-hw-template-master/gcash-mini-hw-template-master/user-management/src/main/java/ph/apper/App.java
@@ -3,11 +3,9 @@ package ph.apper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import ph.apper.controller.UserController;
-import ph.apper.service.UserService;
 
 @SpringBootApplication()
-@ComponentScan(basePackageClasses = UserController.class)
+@ComponentScan("ph.apper")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
